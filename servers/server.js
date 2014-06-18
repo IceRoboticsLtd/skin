204f74253e429f5e594a65a974be117bb8716aea
wvanheemstra <wvanheemstra@icloud.com>
2014-06-18 23:16:26 +0100

    Updated

---
+++ servers/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+/*
+ * CONFIGS - The Configurations
+ */ 	
+config = require('../configs/server.js');
+var configs = config.configs,
+	server_prefix = configs.server_prefix || 'SKIN';
+
+// PLAYGROUND.. type up what you deem meaningful ;o)
+
