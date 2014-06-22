044858a87ed8020e570e3fbcf69a8d49ae5be63e
icerobotics <w.vanheemstra@icerobotics.com>
2014-06-22 17:30:30 +0100

    Updated

--- servers/server.js
+++ servers/server.js
@@ -8,6 +8,66 @@ var configs = config.configs,
 	server_prefix = configs.server_prefix || 'SKIN';
 
 /*
+ * SERVER - The Server used for shutdown etc
+ * See: https://www.exratione.com/2011/07/running-a-nodejs-server-as-a-service-using-forever/
+ */
+var server = express();
+//Port
+if(typeof configs.server_port === 'undefined'){
+	var server_port = process.env.PORT || 10080;
+}
+else {
+	var server_port = configs.server_port;
+}
+server.listen(server_port);
+console.log(server_prefix + " - Node Version: " + process.version);
+console.log(server_prefix + " - Express server listening on port %d", server_port);
+console.log(server_prefix + " - To shutdown server gracefully type: node prepareForStop.js");
+
+server.get('/prepareForShutdown', function(req, res) {
+	if(req.connection.remoteAddress == "127.0.0.1"
+		|| req.socket.remoteAddress == "127.0.0.1"
+		// 0.4.7 oddity in https only
+		|| req.connection.socket.remoteAddress == "127.0.0.1")
+	{
+		managePreparationForShutdown(function() {
+			// don't complete the connection until the preparation is done.
+			res.statusCode = 200;
+			res.end();
+		});
+	}
+	else {
+		res.statusCode = 500;
+		res.end();
+	}
+});
+
+var managePreparationForShutdown = function(callback) {
+	// perform all the cleanup and other operations needed prior to shutdown,
+	// but do not actually shutdown. Call the callback function only when
+	// these operations are actually complete.
+	try {
+		app_server.close();
+		console.log(server_prefix + " - Shutdown app successful.");
+	}
+	catch(ex) {
+		console.log(server_prefix + " - Shutdown app failed.");
+		console.log(ex);
+	}
+	try {
+		api_server.close();
+		console.log(server_prefix + " - Shutdown api successful.");
+	}
+	catch(ex) {
+		console.log(server_prefix + " - Shutdown api failed.");
+		console.log(ex);
+	}
+	console.log(server_prefix + " - All preparations for shutdown completed.");
+	callback();
+};
+
+
+/*
  * APP - The Application
  */
 var app = express();
@@ -31,10 +91,10 @@ else {
 	var api_port = configs.api_port;
 }
 
-app.listen(app_port, function() {
+var app_server = app.listen(app_port, function() {
 	console.log(server_prefix + " - Express app server listening on port %d in %s mode", app_port, app.settings.env);
 });
 
-api.listen(api_port, function() {
+var api_server = api.listen(api_port, function() {
 	console.log(server_prefix + " - Express api server listening on port %d in %s mode", api_port, api.settings.env);
 });
