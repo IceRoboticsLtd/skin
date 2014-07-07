677abc33d641cbffdfda28c8a2df05de87643c2c
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-07 18:27:29 +0100

    Updated

--- pre-public/js/app/lib.js
+++ pre-public/js/app/lib.js
@@ -1,6 +1,11 @@
+/*
+ * Lib
+ */
 define(['jquery'], function ($) {
+	console.log('SKIN: lib called');
     return {
         getBody: function () {
+        	console.log('SKIN: lib getBody called');       	
             return $('body');
         }
     }
