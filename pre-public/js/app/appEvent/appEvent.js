99d3efec262be4c3942bbe217fa2ee8eb0be41d6
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-07 20:58:11 +0100

    Updated

--- pre-public/js/app/appEvent/appEvent.js
+++ pre-public/js/app/appEvent/appEvent.js
@@ -4,9 +4,10 @@
  */
 var AppEvent123 = 1 << 0;
 var AppEvent456 = 1 << 1;
+var AppEventSubscribe = 1 << 2;
 
 define(['./Base'], function (Base) {
-    console.log('appEvent called'); 
+    console.log('SKIN: appEvent called');
     var _AppEvent = new Base(flag);
 
     // following this example, slightly
@@ -15,14 +16,22 @@ define(['./Base'], function (Base) {
     _AppEvent.raiseEvent = function ( flag ) {
         // Check if AppEvent123 was passed.
         if (flag & AppEvent123) {
+            console.log('SKIN: appEvent123 raised');
             // Run the AppController's show function.
             _AppController.loadApp(123);
         }
         // Check if AppEvent456 was passed.
         if (flag & AppEvent456) {
+            console.log('SKIN: appEvent456 raised');            
             // Run the AppController's show function.
             _AppController.loadApp(456);
         }
+        // Check if AppEventSubscribe was passed
+        if (flag & AppEventSubscribe) {
+            console.log('SKIN: appEventSubscribe raised');            
+            // Subscribe ModelService
+            _AppController.subscribeAppService();
+        }         
     };
     return _AppEvent;
 });
