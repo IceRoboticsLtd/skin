41ad37d15d5a20cb91ae975e5330814f2285ce29
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-07 20:58:22 +0100

    Updated

--- pre-public/js/app/appEvent/Base.js
+++ pre-public/js/app/appEvent/Base.js
@@ -2,9 +2,10 @@
  * AppEventBase
  */
 define(function () {
+    console.log('SKIN: appEventBase called');
     function appEventBase(id) {
         this.id = id;
-    }
+    };
     appEventBase.prototype = {
 		// to do
     };
