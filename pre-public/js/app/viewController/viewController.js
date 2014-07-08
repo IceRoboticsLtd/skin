4fd97b6fd58e69e781d4dd7271bbe6bf073c7a43
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-08 01:01:01 +0100

    Updated

--- pre-public/js/app/viewController/viewController.js
+++ pre-public/js/app/viewController/viewController.js
@@ -3,7 +3,7 @@
  */
 define(['./Base'], function (Base) {
 	console.log('SKIN: viewController called');
-    var _ViewController = new Base('');
+    var _ViewController = new Base('ViewController');
 
     // following this example, slightly
     // http://sandbox.thewikies.com/javascript-mvc-hello-world/index.2.html
