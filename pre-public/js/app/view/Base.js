1708b0bbab5be2178f8af737fb785f430c897d50
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-08 03:24:33 +0100

    Updated

--- pre-public/js/app/view/Base.js
+++ pre-public/js/app/view/Base.js
@@ -10,8 +10,25 @@ define(function () {
 		setViewService: function (viewService) {
 			console.log('CORE: ViewBase setViewService(viewService) called');
     		// The view instance has a property called "viewService"
-    		// created from the viewService.					
+    		// created from the viewService.				
 			this.viewService = viewService;
+		},
+		// A view might have a function that returns the rendered output.
+		output = function() {
+			// Data used to create a template may come from anywhere
+			// but in this case template comes from the inline string.
+			var ourData = '<h1><%= myProperty %></h1>';
+			// Store the instance for reference in the replace function below.
+			var instance = this;
+			// Return the template using the values from the viewService.
+			return ourData.replace(/<%=\s+(.*?)\s+%>/g, function (m,m1) {
+				return instance.viewService[m1];
+			});
+		},
+		// A view might have a function that renders the output.
+		render = function () {
+			// This view renders to the element with the id of "output".
+			document.getElementById('output').innerHTML = this.output();
 		}
     };
     return viewBase;
--- pre-public/js/app/view/view.js
+++ pre-public/js/app/view/view.js
@@ -3,27 +3,10 @@
  */
 define(['./Base'], function (Base) {
     console.log('SKIN: view called');	
-    var _View = new Base(viewService);
+    var _View = new Base('');
 
     // following this example, slightly
     // http://sandbox.thewikies.com/javascript-mvc-hello-world/index.2.html
 
-	// A view might have a function that returns the rendered output.
-	_View.prototype.output = function() {
-		// Data used to create a template may come from anywhere
-		// but in this case template comes from the inline string.
-		var ourData = '<h1><%= myProperty %></h1>';
-		// Store the instance for reference in the replace function below.
-		var instance = this;
-		// Return the template using the values from the viewService.
-		return ourData.replace(/<%=\s+(.*?)\s+%>/g, function (m,m1) {
-			return instance.viewService[m1];
-		});
-	};
-	// A view might have a function that renders the output.
-	_View.prototype.render = function () {
-		// This view renders to the element with the id of "output".
-		document.getElementById('output').innerHTML = this.output();
-	};
     return _View;
 });
