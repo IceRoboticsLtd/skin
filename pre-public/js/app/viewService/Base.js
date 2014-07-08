b7a49b2022f1d5eba41cd867472ef753bb7b8f66
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-08 03:15:18 +0100

    Updated

--- pre-public/js/app/viewService/Base.js
+++ pre-public/js/app/viewService/Base.js
@@ -5,11 +5,15 @@ define(function () {
 	console.log('SKIN: viewServiceBase called');	
     function viewServiceBase(id) {
         this.id = id;
+        this.myProperty = {};         
     };
     viewServiceBase.prototype = {
 		setServiceBus: function (serviceBus) {
 			console.log('SKIN: viewServiceBase setServiceBus(serviceBus) called');		
 			this.serviceBus = serviceBus;
+            // The viewService instance has a property called "myProperty"
+            // created from the serviceBus's "yourProperty".
+            this.myProperty = this.serviceBus.yourProperty;              
 		},     
         subscribe: function (config) {
             console.log('SKIN: viewServiceBase subscribe(config) called'); 
