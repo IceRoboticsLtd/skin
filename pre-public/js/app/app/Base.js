8a857477f6ae30ed78990ff506c03b52db1a8225
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-12 16:45:56 +0100

    Updated

--- pre-public/js/app/app/Base.js
+++ pre-public/js/app/app/Base.js
@@ -5,6 +5,7 @@ define(function () {
     console.log('SKIN: appBase called');	
     function appBase(id) {
         this.id = id;
+        this.keyValuePairs = [];
     };
     appBase.prototype = {
         getTitle: function () {
@@ -14,6 +15,24 @@ define(function () {
         setTitle: function (title) {
             console.log('SKIN: appBase setTitle(title) called');             
             this.title = title;
+        },
+        getValue: function (key) {
+            console.log('SKIN: appBase getValue(key) called'); 
+            // To Do: search for key in keyValuePairs and retrieve its value           
+            // return value;
+        },
+        setValue: function (key, value) {
+            console.log('SKIN: appBase setValue(key, value) called'); 
+            // To Do: search for key in keyValuePairs and updated its value           
+            // value = value;
+        },
+        getKeyValuePairs: function () {
+            console.log('SKIN: appBase getKeyValuePairs() called');             
+            return this.keyValuePairs;
+        },
+        setKeyValuePairs: function (keyValuePairs) {
+            console.log('SKIN: appBase setKeyValuePairs(keyValuePairs) called');         
+            this.keyValuePairs = keyValuePairs;
         }
     };
     return appBase;
