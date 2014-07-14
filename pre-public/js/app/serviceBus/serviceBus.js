e37b168e0f2e2cac449c63569f490acd34874883
icerobotics <w.vanheemstra@icerobotics.com>
2014-07-14 15:02:38 +0100

    Updated

--- pre-public/js/app/serviceBus/serviceBus.js
+++ pre-public/js/app/serviceBus/serviceBus.js
@@ -132,24 +132,115 @@ define(['./Base'], function (Base) {
         return Conduit.call(this, options);
     };
 
+    console.log('******** I AM HERE: line BBB **********'); 
+
+    /*
+     * Channel
+     */
+    var ChannelDefinition = function (channelName) {
+        console.log('SKIN: ServiceBusBase ChannelDefinition(channelName) called');  
+    //    this.channel = channelName || this.configuration.DEFAULT_CHANNEL;
+    //    this.initialize();
+    };
+    console.log('******** I AM HERE: line BB **********');    
+    ChannelDefinition.prototype.initialize = function () {
+        console.log('SKIN: ServiceBusBase ChannelDefinition.prototype.initialize() called');
+        // to do
+    };
+    console.log('******** I AM HERE: line CC **********');    
+    ChannelDefinition.prototype.subscribe = function () {
+        console.log('SKIN: ServiceBusBase ChannelDefinition.prototype.subscribe() called');
+        // to do
+    };
+    console.log('******** I AM HERE: line DD **********');    
+    ChannelDefinition.prototype.publish = function () {
+        console.log('SKIN: ServiceBusBase ChannelDefinition.prototype.publish() called');
+        // to do
+    };
+
+    console.log('******** I AM HERE: line CCC **********'); 
+
+    /*
+     * Subscription
+     */
+    var SubscriptionDefinition = function (channel, topic, callback) {
+        console.log('SKIN: ServiceBusBase SubscriptionDefinition(channel, topic, callback) called');
+        if (arguments.length !== 3) {
+            throw new Error("You must provide a channel, topic and callback when creating a SubscriptionDefinition instance.");
+        }
+        if (topic.length === 0) {
+            throw new Error("Topics cannot be empty");
+        }
+        this.channel = channel;
+        this.topic = topic;
+        this.subscribe(callback);
+    };
+    console.log('******** I AM HERE: line DDD **********');    
+    SubscriptionDefinition.prototype = {
+
+        // to do
+
+        subscribe: function (callback) {
+            // to do
+        }
+
+        // to do
+    };
+
+    /*
+     * Variables
+     */
+    var bindingsResolver = {
+        cache: {},
+        regex: {},
+        compare: function (binding, topic) {
+            // to do
+        },
+        reset: function () {
+            this.cache = {};
+            this.regex = {};
+        }
+    }//oef bindingsResolver
+
+    console.log('******** I AM HERE: line EEE **********'); 
+
     /*
      * Functions
      */
     function subscribe(options) {
-    	console.log('SKIN: ServiceBusBase subscribe(options) called');
-        var subDef = new SubscriptionDefinition(options.channel || _ServiceBus.configuration.DEFAULT_CHANNEL, options.topic, options.callback);    	
-		// to do
+        console.log('SKIN: ServiceBusBase subscribe(options) called');
+        var subDef = new SubscriptionDefinition(options.channel || _ServiceBus.configuration.DEFAULT_CHANNEL, options.topic, options.callback);     
+        // to do
 
         return subDef;
     };
+    console.log('******** I AM HERE: line FFF **********');
+
+
+    /*
+     * ServiceBus Properties
+     */
+    _ServiceBus.configuration = {
+        resolver: this.bindingsResolver,
+        DEFAULT_CHANNEL: "/",
+        SYSTEM_CHANNEL: "serviceBus"
+    };
+    _ServiceBus.subscriptions = {};
+    _ServiceBus.wireTaps = [];
+    _ServiceBus.ChannelDefinition = ChannelDefinition;
+    _ServiceBus.SubscriptionDefinition = SubscriptionDefinition;
+
+    console.log('******** I AM HERE: line GGG **********');
 
     /*
      * ServiceBus Functions
-	 */
-	_ServiceBus.subscribe = new Conduit.Sync({
+     */
+    _ServiceBus.subscribe = new Conduit.Sync({
         target: subscribe,
         context: _ServiceBus
     });
 
+
+    console.log('******** I AM HERE: line ZZZ **********');
     return _ServiceBus;
 });
